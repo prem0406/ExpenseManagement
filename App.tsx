bea43251dbb5c9fb1247d6f08710d841a6775a1a
Prem Kumar <prem.kumar@nagarro.com>
2023-05-23 11:56:20 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <Text>Helooss</Text>
+    </>
+  );
+}
+
+export default App;
