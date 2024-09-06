e281ad858c836e69b25a0c4f7cc1791840d119b0
Prem Kumar <v-prem.1.kumar@intra.riyadbank.com>
2024-09-07 01:40:07 +0300

    text types

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -8,6 +8,13 @@ import {
 import React, {useMemo} from 'react';
 import {$fontWeightStyles, Weights} from './text.types';
 import {TextColors, colors} from '../theme/colors';
+import {typography} from '../theme/typography';
+
+export enum TextTypes {
+  body = 'body',
+  body_small = 'body_small',
+  h4 = 'h4',
+}
 
 export interface TextProps extends RNTextProps {
   //   preset?: TextTypes;
@@ -16,9 +23,16 @@ export interface TextProps extends RNTextProps {
   children?: React.ReactNode;
   weight?: Weights;
   color?: TextColors;
+  type?: TextTypes;
 }
 
-export const CustomText = ({children, weight = 'normal', color}: TextProps) => {
+export const CustomText = ({
+  children,
+  weight = 'normal',
+  color,
+  type,
+}: TextProps) => {
+  //weight will be removed
   const $weight = useMemo(() => {
     return $fontWeightStyles[weight as Weights];
   }, [weight]);
@@ -28,13 +42,34 @@ export const CustomText = ({children, weight = 'normal', color}: TextProps) => {
     [color],
   );
 
-  return <Text style={[styles.textStyle, $weight, $color]}>{children}</Text>;
+  const $type = useMemo(() => typeStyles[type ?? TextTypes.body], [type]);
+
+  return (
+    <Text style={[styles.textStyle, $type, $weight, $color]}>{children}</Text>
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 24,
+    fontSize: 10,
     fontWeight: '500',
-    lineHeight: 24,
+    lineHeight: 12,
   },
 });
+
+const typeStyles: Record<keyof typeof TextTypes, TextStyle> = {
+  [TextTypes.body]: {
+    fontFamily: typography.fonts.openSans.bold,
+    fontWeight: '700',
+    ...typography.FONT_STYLES.fontStyle_16,
+  },
+  [TextTypes.body_small]: {
+    //TO be changed
+    ...typography.FONT_STYLES.fontStyle_12,
+  },
+  [TextTypes.h4]: {
+    //TO be changed
+    ...typography.FONT_STYLES.fontStyle_20,
+    fontWeight: '700',
+  },
+};
